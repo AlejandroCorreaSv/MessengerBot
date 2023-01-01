94736ea6383729c138075435b882c755a7a334d7
Alejandro <correasvenssonalejandro1a@gmail.com>
2023-01-01 21:57:00 +0100

    Version 2.0 - New interface and filenames

--- Interfaz_farmacia.py
+++ Interfaz_farmacia.py
@@ -6,10 +6,12 @@
 from tkinter import *
 import tkinter as tk
 import os
+from PIL import Image, ImageTk
 
 #Parametros de configuración de la GUI
 NAME_WINDOW="Mapa Farmacia"
 FONT_BUTTON="Roman 10 italic bold"
+
 
 #EVENTOS de los botones de la GUI
 #Botón Goto que se encarga de lanzar el script Goto.py
@@ -21,43 +23,46 @@ def Goto():
 #argumento por la línea de comandos el número de coordenada correspondiente (1)
 def punto1():
 
-    os.system('gnome-terminal -x python3 leerFichero_trayectoria.py 0')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 leerFichero.py 0')   #lanzamos el leerFichero
 
 #Botón punto2 que se encarga de lanzar el script leerFichero pasandole como 
 #argumento por la línea de comandos el número de coordenada correspondiente (2)
 def punto2():
 
-    os.system('gnome-terminal -x python3 leerFichero_trayectoria.py 1')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 leerFichero.py 1')   #lanzamos el leerFichero
 
 #Botón punto3 que se encarga de lanzar el script leerFichero pasandole como 
 #argumento por la línea de comandos el número de coordenada correspondiente (3)
 def punto3():
 
-    os.system('gnome-terminal -x python3 leerFichero_trayectoria.py 2')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 leerFichero.py 2')   #lanzamos el leerFichero
 
 #Botón punto4 que se encarga de lanzar el script leerFichero pasandole como 
 #argumento por la línea de comandos el número de coordenada correspondiente (4)
 def punto4():
 
-    os.system('gnome-terminal -x python3 leerFichero_trayectoria.py 3')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 leerFichero.py 3')   #lanzamos el leerFichero
 
 #Botón trayectoria1 que se encarga de lanzar el script leerFichero pasandole como 
 #argumentos por la línea de comandos los números de coordenadas correspondientes (0-1-2)
 def trayectoria1():
 
-    os.system('gnome-terminal -x python3 leerFichero_trayectoria.py 0 1 2')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 leerFichero.py 0 1 2')   #lanzamos el leerFichero
 
 #Botón trayectoria2 que se encarga de lanzar el script leerFichero pasandole como 
 #argumentos por la línea de comandos los números de coordenadas correspondientes (0-1-2)
 def trayectoria2():
 
-    os.system('gnome-terminal -x python3 2 leerFicheroTrayectoria.py 1 2 0')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 2 leerFichero.py 1 2 0')   #lanzamos el leerFichero
 
 #Botón trayectoria3 que se encarga de lanzar el script leerFichero pasandole como 
 #argumentos por la línea de comandos los números de coordenadas correspondientes (0-1-2)
 def trayectoria3():
 
-    os.system('gnome-terminal -x python3 3 leerFicheroTRayectoria.py 2 1 0')   #lanzamos el leerFichero
+    os.system('gnome-terminal -x python3 3 leerFichero.py 2 1 0')   #lanzamos el leerFichero
+
+
+
 
 
 
@@ -65,15 +70,22 @@ def trayectoria3():
 #VENTANA PRINCIPAL
 window=Tk()
 window.title(NAME_WINDOW)       #le asignamos un titulo a la ventana principal
-window.geometry("900x900")      #le asignamos un tamaño a la ventana principal
-window.resizable(1,1)           #le permitimos que se pueda redimensionar la ventana
+window.geometry("750x750")      #le asignamos un tamaño a la ventana principal
+
 
 
 #ADD IMAGE FILE; establecemos la imagen del mapa como fondo de la ventana
-background= PhotoImage(file="house.pgm")
-background_zoom=background.zoom(2)
-labelImage= Label(window, image=background_zoom)
-labelImage.place(x=0, y=0)
+"""image = Image.open('background.pgm')
+copy_of_image = image.copy()
+background_img = ImageTk.PhotoImage(image)"""
+background_img = PhotoImage(file = "background.pgm")
+background_img = background_img.zoom(2)
+background = Label(window, image=background_img)
+
+#background.pack(fill=BOTH, expand=YES)
+#background.bind('<Configure>', resize_image)
+background.place(x=0, y=0)
+
 
 #ADD BUTTON; añadimos el boton correspondiente al Goto en el frame1
 frame1=Frame(window)
@@ -116,3 +128,4 @@ but_tray3.grid(row=0, column=3)
 
 #Se muestra la pantalla y se espera a que el usuario interactue con ella
 window.mainloop()
+
